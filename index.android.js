bb69062753074dbf2f9382d90511393245a55b4f
Hero Liu <Hero Liu>
2017-04-17 15:49:46 +0800

    包包风景页面布局完成

--- index.android.js
+++ index.android.js
@@ -4,7 +4,9 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -13,11 +15,37 @@ import {
   Image,
 } from 'react-native';
 
+const Dimensions = require('Dimensions');
+
+let {
+  width
+} = Dimensions.get('window');
+
+//定义全局变量
+const cols = 3;
+const boxWidth = 60;
+const boxHeight = 80;
+const boxLeftMargin = (width - cols * boxWidth) / (cols - 1);
+const boxTopMargin = 20;
+
 export default class AwesomeProject extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.imgContainer}>
+        <View style={styles.beautyContainer}>
+          <Image source={require('./imgs/cell_1.jpg')} style={styles.img}/>
+          <Text style={styles.text}>包包</Text>
+        </View>
+
+        <View style={styles.beautyContainer}>
+          <Image source={require('./imgs/cell_1.jpg')} style={styles.img}/>
+          <Text style={styles.text}>包包</Text>
+        </View>
+        <View style={styles.beautyContainer}>
+          <Image source={require('./imgs/cell_1.jpg')} style={styles.img}/>
+          <Text style={styles.text}>包包</Text>
+        </View>
+        <View style={styles.beautyContainer}>
           <Image source={require('./imgs/cell_1.jpg')} style={styles.img}/>
           <Text style={styles.text}>包包</Text>
         </View>
@@ -28,12 +56,29 @@ export default class AwesomeProject extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    //确定主轴方向
+    flexDirection: 'row',
+    //换行显示
+    flexWrap: 'wrap',
   },
-  
+  beautyContainer: {
+    //设置侧轴对齐方式
+    alignItems: 'center',
+    width: boxWidth,
+    height: boxHeight,
+    marginLeft: boxLeftMargin,
+    marginTop: boxTopMargin,
+  },
+  img: {
+    alignSelf: 'center',
+    width: 100,
+    height: 60,
+  },
+  text: {
+    alignSelf: 'center',
+    color: 'orangered',
+    fontSize: 13,
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
