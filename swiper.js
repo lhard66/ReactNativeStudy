f208c1581d422948ea458707a88ef0b12cd6f276
Hero Liu <lhard@qq.com>
2017-06-03 12:02:23 +0800

    添加注释说明

--- swiper.js
+++ swiper.js
@@ -16,12 +16,14 @@ const {
 } = require('Dimensions').get('window');
 
 class SwiperView extends Component {
+	//变量state初始化
 	constructor(props) {
 		super(props);
 		this.state = {
 			currentPage: 0
 		};
 	}
+	//常量props初始化
 	static defaultProps = {
 		duration: 5000
 	}
@@ -51,10 +53,11 @@ class SwiperView extends Component {
 		}
 		return images;
 	}
-
+	//应用加载完成调用
 	componentDidMount() {
 		this.startTimer();
 	}
+	//对时间组件做防异常处理
 	componentWillUnmount() {
 		this.timer && clearTimeout(this.timer);
 	}
@@ -64,12 +67,14 @@ class SwiperView extends Component {
 		let num = 0;
 		this.timer = setInterval(() => {
 			let activePage = 0;
+			//state变量调用方式
 			console.log(this.state.currentPage);					
 			if ((this.state.currentPage + 1) >= imgCount) {
 				activePage = 0;
 			} else {
 				activePage = this.state.currentPage + 1;
 			}
+			//state变量设值方式
 			this.setState({
 				currentPage: activePage
 			});
@@ -80,6 +85,7 @@ class SwiperView extends Component {
 				y: 0,
 				animated: true
 			});
+			//常量调用方式
 		}, this.props.duration);
 	}
 };
