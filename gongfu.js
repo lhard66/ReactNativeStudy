1b03e720503af0e430bace6ad0d4cc3285cf2793
Hero Liu <lhard@qq.com>
2017-06-07 12:04:59 +0800

    添加八段锦json

--- gongfu.js
+++ gongfu.js
@@ -13,7 +13,21 @@ class Gongfu extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>gongfu</Text>
+				<ListView
+					dataSource={}							//数据源
+					renderRow{this.renderRow}	//渲染布局
+				/>
+			</View>
+		);
+	}
+	renderRow(rowData,sectionID,rowID,highlightRow){
+		reutrn(
+			<View style={styles.cellViewStyle}>
+    		<Image source={{uri:rowData.img}} style={styles.imgStyle} />
+    		<View style={style.wordStyle}>
+    			<Text style={styles.titleStyle}>123456</Text>
+    			<Text style={styles.infoStyle}>abcdefg</Text>
+    		</View>
 			</View>
 		);
 	}
