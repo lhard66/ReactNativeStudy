22212b4d7b26ac96fbf537c3726d5b55b75b5169
Hero Liu <lhard@qq.com>
2017-06-08 19:01:14 +0800

    listview可显示数据“

--- gongfu.js
+++ gongfu.js
@@ -6,25 +6,39 @@ import {
 	StyleSheet,
 	View,
 	Text,
-	ListView
+	Image,
+	ListView,
 } from 'react-native'
 
+const gfData = require('./data/gongfu.json');
+
 class Gongfu extends Component {
+	constructor(props) {
+		super(props);
+		let ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		});
+		this.state = {
+			dataSource:ds.cloneWithRows(gfData.data),
+		}
+	}
 	render() {
-		return (
+		console.log(this.state.dataSource);		
+		return (			
 			<View style={styles.container}>
 				<ListView
-					dataSource={}							//数据源
-					renderRow{this.renderRow}	//渲染布局
-				/>
-			</View>
+					dataSource={this.state.dataSource}//数据源
+					renderRow={this.renderRow}	//渲染布局
+				>					
+				</ListView>
+			</View>				
 		);
 	}
-	renderRow(rowData,sectionID,rowID,highlightRow){
-		reutrn(
+	renderRow(rowData, sectionID, rowID, highlightRow) {
+		return(
 			<View style={styles.cellViewStyle}>
     		<Image source={{uri:rowData.img}} style={styles.imgStyle} />
-    		<View style={style.wordStyle}>
+    		<View style={styles.wordStyle}>
     			<Text style={styles.titleStyle}>123456</Text>
     			<Text style={styles.infoStyle}>abcdefg</Text>
     		</View>
