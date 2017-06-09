1cac39d9b9ae89935cd6953f0015fe731c931f13
Hero Liu <lhard@qq.com>
2017-06-09 14:37:11 +0800

    listview 九宫格

--- share.js
+++ share.js
@@ -15,24 +15,47 @@ import {
 	Alert,
 } from 'react-native';
 
-// const shareData = require('./data/shareData.json');
+const shareData = require('./data/cell.json');
 const {
 	width
 } = require('Dimensions').get('window');
+let cols = 2;
+let cellWidth = 100;
+let vMargin = (width - cellWidth * cols) / (cols + 1);
+let hMargin = 25;
 
 class Share extends Component {
+	constructor(props) {
+			super(props);
+			let ds = new ListView.DataSource({
+				rowHasChanged: (r1, r2) => r1 !== r2
+			});
+			this.state = {
+				dataSource: ds.cloneWithRows(shareData.data),
+			}
+		}
+		/*static defaultProps={
+			clos:3,
+			cellWidth:100,
+
+		}*/
 	render() {
-		return(
+		return (
 			<View style={styles.container}>
-				<Text>123</Text>
+				<ListView
+					dataSource={this.state.dataSource}
+					renderRow={this.renderRow}
+					contentContainerStyle={styles.listViewStyle}
+				>					
+				</ListView>
 			</View>
 		)
 	}
 	renderRow(rowData) {
 		return (
 			<View style={styles.shareContainer}>
-				<Image source={{uri:'cell_1'}} style={styles.img} />
-				<Text style={styles.title}>123</Text>
+				<Image source={{uri:rowData.icon}} style={styles.img} />
+				<Text style={styles.title}>{rowData.name}</Text>
 			</View>
 		);
 	}
@@ -42,14 +65,23 @@ const styles = StyleSheet.create({
 	container: {
 
 	},
+	listViewStyle: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',//主轴上占满时换行
+	},
 	shareContainer: {
-
+		width:100,
+		height:70,
+		marginLeft: vMargin,
+		marginTop: hMargin,
+		alignItems:'center',//在侧轴上对齐
 	},
 	img: {
-
+		width: 100,
+		height: 60,
 	},
 	title: {
-
+		color:'red',
 	}
 });
 
